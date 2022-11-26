6304e45d297267a3bec5c957b54375d1c37c9727
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2022-11-26 12:16:35 -0800

    add-two-numbers

--- add-two-numbers/add-two-numbers_test.go
+++ add-two-numbers/add-two-numbers_test.go
@@ -1,8 +1,9 @@
 package addtwonumbers
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 // https://leetcode.com/problems/add-two-numbers/
@@ -15,10 +16,10 @@ type ListNode struct {
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	l1Ptr := l1
 	l2Ptr := l2
-	sumPtr := &ListNode{Val: 0, Next: nil}
-	ret := sumPtr
+	prevSumPtr := &ListNode{Val: 0, Next: nil}
+	startPtr := prevSumPtr
 	carry := 0
-	for l1Ptr.Next != nil || l2Ptr.Next != nil {
+	for l1Ptr != nil || l2Ptr != nil {
 		l1Addend := 0
 		if l1Ptr != nil {
 			l1Addend = l1Ptr.Val
@@ -31,16 +32,18 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		}
 		sum := l1Addend + l2Addend + carry
 		if sum > 9 {
-			sum = 10 - sum
+			sum = sum - 10
 			carry = 1
 		} else {
 			carry = 0
 		}
-		sumPtr.Val = sum
-		sumPtr.Next = &ListNode{Val: 0, Next: nil}
-		// TODO: advance ptr to next; also use prevSumPtr instead of sumptr
+		prevSumPtr.Next = &ListNode{Val: sum, Next: nil}
+		prevSumPtr = prevSumPtr.Next
 	}
-	return ret
+	if carry == 1 {
+		prevSumPtr.Next = &ListNode{Val: 1, Next: nil}
+	}
+	return startPtr.Next
 }
 
 func Test_addTwoNumbers(t *testing.T) {
@@ -49,17 +52,41 @@ func Test_addTwoNumbers(t *testing.T) {
 		l2 *ListNode
 	}
 	tests := []struct {
-		name string
-		args args
-		want *ListNode
+		name     string
+		args     args
+		expected *ListNode
 	}{
+		{
+			name: "carry",
+			args: args{
+				l1: &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, nil}}}}}}},
+				l2: &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, nil}}}},
+			},
+			expected: &ListNode{8, &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{0, &ListNode{0, &ListNode{0, &ListNode{1, nil}}}}}}}},
+		},
+		{
+			name: "carry",
+			args: args{
+				l1: &ListNode{Val: 9, Next: nil},
+				l2: &ListNode{Val: 1, Next: nil},
+			},
+			expected: &ListNode{Val: 0, Next: &ListNode{Val: 1, Next: nil}},
+		},
+		{
+			name: "b2",
+			args: args{
+				l1: &ListNode{Val: 1, Next: nil},
+				l2: &ListNode{Val: 2, Next: nil},
+			},
+			expected: &ListNode{Val: 3, Next: nil},
+		},
 		{
 			name: "1",
 			args: args{
 				l1: &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3, Next: nil}}},
 				l2: &ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4, Next: nil}}},
 			},
-			want: &ListNode{Val: 7, Next: &ListNode{Val: 0, Next: &ListNode{Val: 8, Next: nil}}},
+			expected: &ListNode{Val: 7, Next: &ListNode{Val: 0, Next: &ListNode{Val: 8, Next: nil}}},
 		},
 		{
 			name: "2",
@@ -67,14 +94,16 @@ func Test_addTwoNumbers(t *testing.T) {
 				l1: &ListNode{Val: 0, Next: nil},
 				l2: &ListNode{Val: 0, Next: nil},
 			},
-			want: &ListNode{Val: 0, Next: nil},
+			expected: &ListNode{Val: 0, Next: nil},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := addTwoNumbers(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("addTwoNumbers() = %v, want %v", got, tt.want)
-			}
+			// if got := addTwoNumbers(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.expected) {
+			// 	t.Errorf("addTwoNumbers() = %v, want %v", got, tt.expected)
+			// }
+			actual := addTwoNumbers(tt.args.l1, tt.args.l2)
+			require.Equal(t, tt.expected, actual)
 		})
 	}
 }
