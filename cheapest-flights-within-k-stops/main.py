523b4697e15adb820a6b62f2b4befc48298f8acc
Benjamin Kane <92759008+bkane-msft@users.noreply.github.com>
2023-02-01 15:52:55 -0800

    haha git go brrr

--- cheapest-flights-within-k-stops/main.py
+++ cheapest-flights-within-k-stops/main.py
@@ -12,10 +12,9 @@ class Flight(NamedTuple):
     price: int
 
 
-@dataclasses.dataclass
-class Node:
+class Node(NamedTuple):
     flight: Flight
-    hops: int
+    total_hops: int
     total_price: int
 
 
@@ -28,6 +27,9 @@ class PriorityQueue:
         self.entry_finder = {}  # mapping of tasks to entries
         self.REMOVED = "<removed-task>"  # placeholder for a removed task
         self.counter = itertools.count()  # unique sequence count
+
+    def empty(self):
+        return len(self.pq) == 0
 
     def add_task(self, task, priority=0):
         "Add a new task or update the priority of an existing task"
@@ -43,15 +45,14 @@ class PriorityQueue:
         entry = self.entry_finder.pop(task)
         entry[-1] = self.REMOVED
 
-
-def pop_task(self):
-    "Remove and return the lowest priority task. Raise KeyError if empty."
-    while self.pq:
-        priority, count, task = heappop(self.pq)
-        if task is not self.REMOVED:
-            del self.entry_finder[task]
-            return task
-    raise KeyError("pop from an empty priority queue")
+    def pop_task(self):
+        "Remove and return the lowest priority task. Raise KeyError if empty."
+        while self.pq:
+            priority, count, task = heappop(self.pq)
+            if task is not self.REMOVED:
+                del self.entry_finder[task]
+                return task
+        raise KeyError("pop from an empty priority queue")
 
 
 class Solution:
@@ -66,10 +67,57 @@ class Solution:
 
         # priority queue!
         pq = PriorityQueue()
+        # Add first one
         pq.add_task(
             Node(
                 flight=Flight(to=src, price=0),
-                hops=0,
+                total_hops=0,
                 total_price=0,
             ),
+            priority=0,
         )
+        while not pq.empty():
+            current = pq.pop_task()
+            if current.flight.to == dst:
+                return current.total_price
+
+            if current.total_hops > k:
+                continue
+            for child in graph[current.flight.to]:
+                pq.add_task(
+                    Node(
+                        flight=child,
+                        total_hops=current.total_hops + 1,
+                        total_price=current.total_price + child.price,
+                    ),
+                    priority=current.total_price + child.price,
+                )
+        return -1
+
+
+s = Solution()
+
+n = 4
+flights = [[0, 1, 100], [1, 2, 100], [2, 0, 100], [1, 3, 600], [2, 3, 200]]
+src = 0
+dst = 3
+k = 1
+
+assert s.findCheapestPrice(n, flights, src, dst, k) == 700
+
+
+n = 3
+flights = [[0, 1, 100], [1, 2, 100], [0, 2, 500]]
+src = 0
+dst = 2
+k = 1
+solution = s.findCheapestPrice(n, flights, src, dst, k)
+print(solution)
+assert solution == 200
+
+n = 3
+flights = [[0, 1, 100], [1, 2, 100], [0, 2, 500]]
+src = 0
+dst = 2
+k = 0
+assert s.findCheapestPrice(n, flights, src, dst, k) == 500
